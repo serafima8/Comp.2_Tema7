c54e2f4de47ec7dad320e5b488f285195fb506ef
serafima8 <147659413+serafima8@users.noreply.github.com>
2023-10-24 23:41:11 +0300

    Add files via upload

---
+++ Тема 7, комп.практикум 2/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача 1.Дано название футбольного клуба.Определить количество символов в нем.
+            //Console.Write("Введите название футбольного клуба: ");
+            //string club = Console.ReadLine();
+            //int characterCount = club.Length;
+            //Console.WriteLine($"Количество символов в названии = {characterCount}");
+
+
+            //Задача 2.Дано название города. Определить, четно или нет количество символов в нем.
+            //Console.Write("Введите название города: ");
+            //string city = Console.ReadLine();
+            //int characterCount = city.Length;
+            //if (characterCount % 2 == 0)
+            //{
+            //    Console.WriteLine($"{characterCount} количетво символов в названии чётное");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{characterCount} количетво символов в названии нечётное");
+            //}
+
+
+            //Задача 3.Даны две фамилии. Определить, какая из них длиннее.
+            //Console.Write("Введите первую фамилию: ");
+            //string surname1 = Console.ReadLine();
+            //Console.Write("Введите вторую фамилию: ");
+            //string surname2 = Console.ReadLine();
+            //string compare = surname1.Length > surname2.Length ? "Первая фамилия длиннее" : "Вторая фамилия длиннее";
+            //Console.WriteLine(compare);
+
+
+            //Задача 4.Найти самое длинное слово из пяти введенных с клавиатуры.
+            //string st = " ";
+            //int l = st.Length;
+            //string word = " ";
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.Write($"Введите {i+1}-е слово: ");
+            //    string st1 = Console.ReadLine();
+            //    if (st1.Length > l) 
+            //    {
+            //        l = st1.Length;
+            //        word = st1;
+            //    }
+
+            //}
+            //Console.WriteLine($"Самое длинное слово {word}");
+
+
+            //Задача 5.Составить программу, которая запрашивает отдельно имя и отдельно фамилию, а затем выводит их как одну символьную строку.
+            //Console.Write("Введите имя: ");
+            //string name = Console.ReadLine();
+            //Console.Write("Введите фамилию: ");
+            //string surname = Console.ReadLine();
+            //string st = " ";
+            //string st1 = name + st + surname;
+            //Console.WriteLine(st1);
+
+            Console.ReadKey();
+
+           
+
+
+        }
+    }
+}
